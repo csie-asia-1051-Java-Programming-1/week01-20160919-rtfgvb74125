6b79c8508c1df723d645143b6ac9c7fdda2de8ed
rtfgvb74125 <105021056@live.asia.edu.tw>
2016-09-19 15:27:16 +0800

    ex01

--- src/ex/ex01_105021056.java
+++ src/ex/ex01_105021056.java
@@ -2,14 +2,17 @@ package ex;
 /*
  * Topic: 請寫一個程式讓使用者輸入「華氏」溫度，輸出「攝氏」溫度 (華氏 = 攝氏*(9/5)+32)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021056 王家恩
  */
-
-public class ex01_1050210XX {
+import java.util.Scanner;
+public class ex01_105021056 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Scanner scn = new Scanner(System.in);
+	        int a = scn.nextInt();
+	       
+	        System.out.println(a/(9/5)-32);
 	}
 
 }

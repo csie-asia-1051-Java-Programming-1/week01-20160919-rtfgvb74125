fedf722803e7768e7cdac385c415b8921de55aab
rtfgvb74125 <105021056@live.asia.edu.tw>
2016-09-19 15:46:48 +0800

    ex03繳交

--- src/ex/ex03_105021056.java
+++ src/ex/ex03_105021056.java
@@ -1,15 +1,20 @@
 package ex;
+
+import java.util.Scanner;
 /*
  * Topic: 請寫一個程式讓使用者輸入數字 a 與 b，程式 計算 a 的 b 次方後輸出結果 (請使用 Math.pow(a,b) 工具)
  * Date: 2016/09/19
- * Author: 1050210XX 周永振老師
+ * Author: 105021056 王家恩
  */
 
-public class ex03_1050210XX {
+public class ex03_105021056 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		 Scanner scn = new Scanner(System.in);
+	        int a = scn.nextInt();
+	        int b = scn.nextInt();
+	        System.out.println( Math.pow(a,b));
 	}
 
 }
